2ea7dd3c84eb7278d57db2dfb0dd3862f4a1b5de
AruNi-01 <1298911600@qq.com>
2023-02-09 13:38:12 +0800

    import docsearch

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,6 +3,7 @@ import theme from "./theme.js";
 import {searchPlugin} from "@vuepress/plugin-search";
 import { nprogressPlugin } from '@vuepress/plugin-nprogress';
 import { commentPlugin } from "vuepress-plugin-comment2";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 
 
 //自定义用户配置
@@ -25,19 +26,69 @@ export default defineUserConfig({
 
   plugins: [
     // 搜索插件
-    searchPlugin({
-      //多语言支持
+    // searchPlugin({
+    //   //多语言支持
+    //   locales: {
+    //     "/": {
+    //       placeholder: "搜索 Ctrl+k",
+    //     },
+    //   },
+    //   // 热键支持
+    //   hotKeys: ["command", "k"],
+    //   // 最大推荐个数
+    //   maxSuggestions: 7,
+    //   // 排除首页中的内容
+    //   isSearchable: (page) => page.path !== "/",
+    // }),
+    docsearchPlugin({
+      appId: "5BC4BBMT90",
+      apiKey: "0f8789cd48dbcc5ae98b33d8172fae79",
+      indexName: "aruni",
+
       locales: {
         "/": {
-          placeholder: "搜索 Ctrl+k",
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
         },
       },
-      // 热键支持
-      hotKeys: ["command", "k"],
-      // 最大推荐个数
-      maxSuggestions: 7,
-      // 排除首页中的内容
-      isSearchable: (page) => page.path !== "/",
+
     }),
 
     // 评论插件
