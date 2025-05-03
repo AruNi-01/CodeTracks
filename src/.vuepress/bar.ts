5927c8cd28e688711059f9175ba44d13ee4cf963
lurunrun820 <lurunrun820@hellobike.com>
2025-05-03 10:14:59 +0800

    feat: up version

--- src/.vuepress/bar.ts
+++ src/.vuepress/bar.ts
@@ -4,7 +4,7 @@ export const Navbar = navbar([
     { text: "后端足迹", icon: "backend", link: "/backend/" },
     { text: "前端足迹", icon: "frontend", link: "/frontend/" },
     { text: "项目作品", icon: "project", link: "/project/" },
-    { text: "人生杂谈", icon: "rensheng", link: "/lifetalk/" },
+    // { text: "人生杂谈", icon: "rensheng", link: "/lifetalk/" },
     { text: "链接分享", icon: "link", link: "/link/" },
     { text: "个人主页", icon: "personal_website", link: "https://0x3f4.run" },
 ]);
@@ -13,7 +13,7 @@ export const Sidebar = sidebar({
 
     "/backend/": "structure",
     "/frontend/": "structure",
-    "/lifetalk/": "structure",
+    // "/lifetalk/": "structure",
 
     
     // ========================= 学习笔记 =========================
